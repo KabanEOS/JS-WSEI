0d8b5a0a881e9c019335ca4bda6bb13dab62cf7e
Michał Madejski <49588850+KabanEOS@users.noreply.github.com>
2020-10-18 12:40:47 +0200

    ts still the hell of work :qw

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -4,17 +4,88 @@
 //
 // console.log(myTable[0])
 
-//Zadaanie 2
-
-// let myTable = [1, 2, 3, 4, 5];
+//Zadanie 2
 //
-// // console.log(myTable[0])
-// // console.log(myTable[myTable.length - 1]);
-// // console.log(myTable);
-// for (i=0; i<myTable.length; i+= 2
-// ) {
-//     console.log(myTable[i])
+// let myTable = [1, 2, 3, 4, 'aaa', 'bbbb'];
+//
+// console.log(myTable[0]);
+// console.log(myTable[myTable.length-1]);
+// console.log(myTable);
+//
+// for(let i=0; i<myTable.length; i++){
+//     if(i%2 == 0){
+//         console.log(myTable[i])
+//     }
+//     if(typeof(myTable[i]) == 'string') {
+//         console.log(myTable[i])
+//     }
+//     if(typeof(myTable[i]) == 'number') {
+//         console.log(myTable[i])
+//     }
 // }
 
-//Zadanie 3
+// Zadanie 4
+//
+//TODO zrozumieć
+//
+// const arr = [42, 32, 12, 53, 12, 51];
+//
+// // 1
+// console.log(arr.reduce((a, b) => a + b));
+// // 2
+// console.log(arr.reduce((a, b) => a - b));
+// // 3
+// console.log(arr.reduce((a, b) => a + b / arr.length));
+// // 4
+// arr.filter((v, i) => v % 2 === 0).forEach(v => {
+//     console.log(v);
+// });
+// // 5
+// arr.filter((v, i) => v % 2 !== 0).forEach(v => {
+//     console.log(v);
+// });
+// // 6
+// let biggest = 0;
+// arr.forEach(v => {
+//     if (v > biggest) {
+//         biggest = v;
+//     }
+// });
+// console.log(biggest);
+// // 7
+// let smallest = 0;
+// arr.forEach(v => {
+//     if (v < smallest) {
+//         smallest = v;
+//     }
+// });
+// console.log(smallest);
+// // 8
+// const arrLength = arr.length - 1;
+// arr.forEach((v, i) => {
+//     console.log(arr[arrLength - i]);
+// });
+//
+//
 
+// Zadanie 5
+
+function myTable(paramTable){
+
+    for(i=0; i<paramTable.length; i++){
+        console.log()
+    }
+};
+
+myTable([1,2,3,4]);
+
+// Zadanie 6
+
+function myTable(paramTable){
+    var sum =0;
+    for (i=0; i>paramTable.lenght; i++){
+        sum = sum + paramTable[i];
+    }
+    var = 
+};
+myTable([1,2,3,4]);
