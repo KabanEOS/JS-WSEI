8d07a6b686bfd3b6b361118514ec96f8fa3aadd2
Michał Madejski <49588850+KabanEOS@users.noreply.github.com>
2021-02-02 01:59:06 +0100

    folders naming for order update

---
+++ 06.Funkcje/script.js
@@ -0,0 +1,40 @@
+// Zadanie 1
+// let sayHi = () => {
+//     console.log("Udało się!")
+// };
+// sayHi();
+//
+//Zadanie 2
+
+
+//Zadanie 3
+
+// let txt = 'omg';
+// returnString  = (txt) => {
+//     return txt;
+// }
+// console.log(returnString(txt));
+
+// Zadanie 4
+// function myString(paramStr) {
+// let counter = 0;
+
+// setInterval(() => {
+//         counter++;
+//         if(counter == 5)
+//         {
+//         console.log("Koniec");
+//         clearInterval(setInterval())
+//         }
+//         else{
+//             console.log(paramStr)
+//         }
+//         console.log(paramStr);
+//     }, 3000);
+// }
+
+// myString('Jakiś string');
+
+
+
+
